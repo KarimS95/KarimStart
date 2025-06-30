1b95d1e2922449ae69cd0d795296019be2e64020
Karim S <krs261095@gmail.com>
2025-06-30 22:36:23 +0300

    homework2 1-st iteration fix commit

--- src/main/HomeWork2/ATM.java
+++ src/main/HomeWork2/ATM.java
@@ -122,45 +122,57 @@ public class ATM {
     //Логика проверки вычисления суммы купюр, необходимых для покрытия суммы amount. Например: Для суммы 1470 EUR нужно использовать 7 купюр по 200, 1 купюру 50 и 1 купюру 20
     public static int amountValidator(BigDecimal amount, Currency currency) {
         int countOfBills = 0;
-        if (amount.remainder(BigDecimal.valueOf(5)).equals(BigDecimal.ZERO)) { //Проверка на возможность внести сумму, с учетом доступных для ввода номиналов купюр: например: сумме в 1471 евро не получится внести, т.к. минимальная возможная купюра, которую банкомат может принять для данной валюты, не покроет всю сумму без остатка
+        //Проверка на возможность внести сумму, с учетом доступных для ввода номиналов купюр: например: сумме в 1471 евро не получится внести, т.к. минимальная возможная купюра, которую банкомат может принять для данной валюты, не покроет всю сумму без остатка
             switch (currency) {
                 case USD:
-                    String[] AVAILABLE_USD_NOMINALS = {"100", "50", "20", "10", "5", "1"};
-                    int[] countUsdBills = new int[AVAILABLE_USD_NOMINALS.length];
-                    for (int i = 0; i < AVAILABLE_USD_NOMINALS.length; i++) {
-                        BigDecimal billValue = new BigDecimal(AVAILABLE_USD_NOMINALS[i]);
-                        countUsdBills[i] = amount.divideToIntegralValue(billValue).intValue();
-                        amount = amount.remainder(billValue);
-                        countOfBills += countUsdBills[i];
+                    if (amount.remainder(BigDecimal.valueOf(1)).equals(BigDecimal.ZERO)) {
+                        String[] AVAILABLE_USD_NOMINALS = {"100", "50", "20", "10", "5", "1"};
+                        int[] countUsdBills = new int[AVAILABLE_USD_NOMINALS.length];
+                        for (int i = 0; i < AVAILABLE_USD_NOMINALS.length; i++) {
+                            BigDecimal billValue = new BigDecimal(AVAILABLE_USD_NOMINALS[i]);
+                            countUsdBills[i] = amount.divideToIntegralValue(billValue).intValue();
+                            amount = amount.remainder(billValue);
+                            countOfBills += countUsdBills[i];
+                        }
+                        break;
+                    } else {
+                        System.err.println("Invalid amount. Enter a correct amount for " + currency);
+                        System.exit(1);
                     }
-                    break;
 
                 case EUR:
-                    String[] AVAILABLE_EUR_NOMINALS = {"200", "100", "50", "20", "10", "5"};
-                    int[] countEurBills = new int[AVAILABLE_EUR_NOMINALS.length];
-                    for (int i = 0; i < AVAILABLE_EUR_NOMINALS.length; i++) {
-                        BigDecimal billValue = new BigDecimal(AVAILABLE_EUR_NOMINALS[i]);
-                        countEurBills[i] = amount.divideToIntegralValue(billValue).intValue();
-                        amount = amount.remainder(billValue);
-                        countOfBills += countEurBills[i];
+                    if (amount.remainder(BigDecimal.valueOf(5)).equals(BigDecimal.ZERO)) {
+                        String[] AVAILABLE_EUR_NOMINALS = {"200", "100", "50", "20", "10", "5"};
+                        int[] countEurBills = new int[AVAILABLE_EUR_NOMINALS.length];
+                        for (int i = 0; i < AVAILABLE_EUR_NOMINALS.length; i++) {
+                            BigDecimal billValue = new BigDecimal(AVAILABLE_EUR_NOMINALS[i]);
+                            countEurBills[i] = amount.divideToIntegralValue(billValue).intValue();
+                            amount = amount.remainder(billValue);
+                            countOfBills += countEurBills[i];
+                        }
+                        break;
+                    } else {
+                        System.err.println("Invalid amount. Enter a correct amount for " + currency);
+                        System.exit(1);
                     }
-                    break;
 
                 case RUB:
-                    String[] AVAILABLE_RUB_NOMINALS = {"5000", "1000", "500", "200", "100", "50"};
-                    int[] countRubBills = new int[AVAILABLE_RUB_NOMINALS.length];
-                    for (int i = 0; i < AVAILABLE_RUB_NOMINALS.length; i++) {
-                        BigDecimal billValue = new BigDecimal(AVAILABLE_RUB_NOMINALS[i]);
-                        countRubBills[i] = amount.divideToIntegralValue(billValue).intValue();
-                        amount = amount.remainder(billValue);
-                        countOfBills += countRubBills[i];
+                    if (amount.remainder(BigDecimal.valueOf(50)).equals(BigDecimal.ZERO)) {
+                        String[] AVAILABLE_RUB_NOMINALS = {"5000", "1000", "500", "200", "100", "50"};
+                        int[] countRubBills = new int[AVAILABLE_RUB_NOMINALS.length];
+                        for (int i = 0; i < AVAILABLE_RUB_NOMINALS.length; i++) {
+                            BigDecimal billValue = new BigDecimal(AVAILABLE_RUB_NOMINALS[i]);
+                            countRubBills[i] = amount.divideToIntegralValue(billValue).intValue();
+                            amount = amount.remainder(billValue);
+                            countOfBills += countRubBills[i];
+                        }
+                        break;
+                    } else {
+                        System.err.println("Invalid amount. Enter a correct amount for " + currency);
+                        System.exit(1);
                     }
-                    break;
             }
-        } else {
-            System.err.println("Invalid amount. Enter a correct amount for " + currency);
-            System.exit(1);
-        }
+
         return countOfBills;
     }
 }
